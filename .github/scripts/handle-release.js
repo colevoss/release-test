46d9f707b7b2402e3a2c9f2e534acbe3c8bbdeaf
Cole Voss <cole.voss@icanbwell.com>
2024-04-17 12:44:22 -0500

    get vars

--- .github/scripts/handle-release.js
+++ .github/scripts/handle-release.js
@@ -1,11 +1,41 @@
 // @ts-check
 
+/**
+ * @typedef {Object} EnvVars - Environment varibles that should be set for this script
+ * @property {string} currentVersion
+ * @property {string} newTag
+ * @property {string=} prevTag
+ */
+
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 module.exports = async ({ github, core, context }) => {
-  const input = core.getInput("PREV_TAG", { required: true });
+  /** @type {EnvVars} */
+  let envVars;
 
-  core.info(`PREV TAG: ${input}`);
-  core.debug("Running something at the moment");
+  try {
+    envVars = getEnvVariables();
+  } catch (e) {
+    core.setFailed(e.message);
+    return;
+  }
 
-  return context.actor;
+  console.log(envVars);
 };
+
+function getEnvVariables() {
+  const { CURRENT_VERSION, NEW_TAG, PREV_TAG } = process.env;
+
+  if (CURRENT_VERSION === undefined) {
+    throw new Error("CURRENT_VERSION environment variable is not present");
+  }
+
+  if (NEW_TAG === undefined) {
+    throw new Error("NEW_TAG environment variable is not present");
+  }
+
+  return {
+    currentVersion: CURRENT_VERSION,
+    newTag: NEW_TAG,
+    prevTag: PREV_TAG,
+  };
+}
