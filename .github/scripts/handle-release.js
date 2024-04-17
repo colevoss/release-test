8b315b7b4ff36dd668e1582888198c81188c0b13
Cole Voss <cole.voss@icanbwell.com>
2024-04-17 13:14:13 -0500

    get previous release tag as well

--- .github/scripts/handle-release.js
+++ .github/scripts/handle-release.js
@@ -2,9 +2,10 @@
 
 /**
  * @typedef {Object} EnvVars - Environment varibles that should be set for this script
+ * @property {string} prevReleaseTag
  * @property {string} currentVersion
  * @property {string} newTag
- * @property {string=} prevTag
+ * @property {string=} prevPreRelease
  */
 
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
@@ -22,11 +23,20 @@ module.exports = async ({ github, core, context }) => {
   console.log(envVars);
 };
 
+/**
+ * Gets variables from environment and validates required variables are present
+ *
+ * @returns {EnvVars}
+ */
 function getEnvVariables() {
-  const { CURRENT_VERSION, NEW_TAG, PREV_TAG } = process.env;
+  const { CURRENT_VERSION, NEW_TAG, PREV_TAG, PREV_RELEASE_TAG } = process.env;
 
   if (CURRENT_VERSION === undefined) {
     throw new Error("CURRENT_VERSION environment variable is not present");
+  }
+
+  if (PREV_RELEASE_TAG === undefined) {
+    throw new Error("PREV_RELEASE_TAG environment variable is not present");
   }
 
   if (NEW_TAG === undefined) {
@@ -34,8 +44,9 @@ function getEnvVariables() {
   }
 
   return {
+    prevReleaseTag: PREV_RELEASE_TAG,
     currentVersion: CURRENT_VERSION,
     newTag: NEW_TAG,
-    prevTag: PREV_TAG,
+    prevPreRelease: PREV_TAG,
   };
 }
